481f8475625127a47dcf774d3fa3dc99d317a871
Vivek <vivekuit344@gmail.com>
2024-10-03 09:27:25 +0530

    Added Tests for account.sql.go

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	dbSource = "postgresql://root:vivek@localhost:5432/banking_system?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to the database", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
